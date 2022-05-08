bd7c0c1b9434d4f202abf572b3354c70b37e6096
VOZ'MAK <strela996@bk.ru>
2022-05-08 18:53:13 +0300

    feat: install button, input, form modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule } from '@nebular/theme';
+import { InputComponent } from './components/input/input.component';
+import { ButtonComponent } from './components/button/button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InputComponent,
+    ButtonComponent,
+  ],
+  exports: [
+    InputComponent,
+    ButtonComponent,
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NbInputModule,
+    NbButtonModule,
+    NbFormFieldModule,
+    NbIconModule,
+    FormsModule,
+  ],
 })
 export class SharedModule { }
