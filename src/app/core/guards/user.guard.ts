e33f84a0262edf1669943da33babf2af2d666cd1
VOZ'MAK <strela996@bk.ru>
2022-05-08 14:21:43 +0300

    feat(guard): create canActivate user guard

---
+++ src/app/core/guards/user.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { ToastrService } from '../services/toastr.service';
+import { ProfileState } from '../store/profile/profile.state';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private toastrService: ToastrService) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const user = this.store.selectSnapshot(ProfileState.user);
+    if (user?.name) {
+      return true;
+    }
+    this.toastrService.showWarningToastr({ message: 'After look profile, register please.' });
+    return this.router.createUrlTree([`profile/register`]);
+  }
+
+}
