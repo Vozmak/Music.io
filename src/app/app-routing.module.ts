da4c23771d36994cb03ead984234eb13d561433b
VOZ'MAK <strela996@bk.ru>
2022-05-09 19:22:45 +0300

    feat: add routing; add width for sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserGuard } from './core/guards/user.guard';
 
 const routes: Routes = [
   {
     path: 'profile',
-    loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule)
+    loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule),
   },
   {
     path: 'home',
-    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'lobbies',
+    canActivate: [UserGuard],
+    loadChildren: () => import('./features/lobbies/lobbies.module').then(m => m.LobbiesModule),
   },
   {
     path: '',
