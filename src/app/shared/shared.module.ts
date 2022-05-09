1857408389c038eae58bb66313b92dd52606baff
VOZ'MAK <strela996@bk.ru>
2022-05-09 19:21:26 +0300

    refactor: install components and modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule } from '@nebular/theme';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule, NbUserModule } from '@nebular/theme';
 import { InputComponent } from './components/input/input.component';
 import { ButtonComponent } from './components/button/button.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { FormGeneratorComponent } from './components/form-generator/form-generator.component';
 
 
 
@@ -11,10 +13,14 @@ import { ButtonComponent } from './components/button/button.component';
   declarations: [
     InputComponent,
     ButtonComponent,
+    UserCardComponent,
+    FormGeneratorComponent,
   ],
   exports: [
     InputComponent,
     ButtonComponent,
+    UserCardComponent,
+    FormGeneratorComponent,
   ],
   imports: [
     CommonModule,
@@ -23,6 +29,8 @@ import { ButtonComponent } from './components/button/button.component';
     NbFormFieldModule,
     NbIconModule,
     FormsModule,
+    NbUserModule,
+    ReactiveFormsModule,
   ],
 })
 export class SharedModule { }
